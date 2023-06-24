b4fef9f695eafc8f40ab5c045bc07c46cf7da7a5
Umer Irfan <54197021+Foxunderground0@users.noreply.github.com>
2023-06-24 16:19:45 +0500

    Modified Code and Added Fucntionality

--- emotions.py
+++ emotions.py
@@ -1,157 +1,108 @@
 import numpy as np
-import time
 import argparse
-import matplotlib.pyplot as plt
 import cv2
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, Flatten
-from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
 from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.layers import MaxPooling2D
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import os
 import face_recognition
+from datetime import datetime
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-# command line argument
+# Command line argument
 ap = argparse.ArgumentParser()
 ap.add_argument("--mode", help="train/display")
 mode = ap.parse_args().mode
-
-# plots accuracy and loss curves
-
-
-def plot_model_history(model_history):
-    """
-    Plot Accuracy and Loss curves given the model_history
-    """
-    fig, axs = plt.subplots(1, 2, figsize=(15, 5))
-    # summarize history for accuracy
-    axs[0].plot(range(1, len(model_history.history['accuracy'])+1),
-                model_history.history['accuracy'])
-    axs[0].plot(range(1, len(model_history.history['val_accuracy'])+1),
-                model_history.history['val_accuracy'])
-    axs[0].set_title('Model Accuracy')
-    axs[0].set_ylabel('Accuracy')
-    axs[0].set_xlabel('Epoch')
-    axs[0].set_xticks(np.arange(1, len(model_history.history['accuracy'])+1),
-                      len(model_history.history['accuracy'])/10)
-    axs[0].legend(['train', 'val'], loc='best')
-    # summarize history for loss
-    axs[1].plot(range(1, len(model_history.history['loss'])+1),
-                model_history.history['loss'])
-    axs[1].plot(range(1, len(model_history.history['val_loss'])+1),
-                model_history.history['val_loss'])
-    axs[1].set_title('Model Loss')
-    axs[1].set_ylabel('Loss')
-    axs[1].set_xlabel('Epoch')
-    axs[1].set_xticks(np.arange(
-        1, len(model_history.history['loss'])+1), len(model_history.history['loss'])/10)
-    axs[1].legend(['train', 'val'], loc='best')
-    fig.savefig('plot.png')
-    plt.show()
-
+mode = "directory"  # Default mode
 
 num_train = 28709
 num_val = 7178
 batch_size = 64
 num_epoch = 50
 
-train_datagen = ImageDataGenerator(rescale=1./255)
-val_datagen = ImageDataGenerator(rescale=1./255)
-
-
 # Create the model
 model = Sequential()
-
 model.add(Conv2D(32, kernel_size=(3, 3),
           activation='relu', input_shape=(48, 48, 1)))
 model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
-
 model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
-
 model.add(Flatten())
 model.add(Dense(1024, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(7, activation='softmax'))
 
-# If you want to train the same model or try other models, go for this
 if mode == "train":
-    model.compile(loss='categorical_crossentropy', optimizer=Adam(
-        lr=0.0001, decay=1e-6), metrics=['accuracy'])
-    model_info = model.fit_generator(
-        train_generator,
-        steps_per_epoch=num_train // batch_size,
-        epochs=num_epoch,
-        validation_data=validation_generator,
-        validation_steps=num_val // batch_size)
-    plot_model_history(model_info)
-    model.save_weights('model.h5')
-
-# emotions will be displayed on your face from the webcam feed
-elif mode == "display":
+    print("Not Available")
+elif mode == "video":
     model.load_weights('model.h5')
-
-    # prevents openCL usage and unnecessary logging messages
     cv2.ocl.setUseOpenCL(False)
-
-    # dictionary which assigns each label an emotion (alphabetical order)
     emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful",
                     3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
-
-    # start the webcam feed
-    cap = cv2.VideoCapture("a.mp4")
+    cap = cv2.VideoCapture("e.mp4")
     while True:
-        # Find haar cascade to draw bounding box around face
         ret, frame = cap.read()
-        frame = cv2.imread("e.jpg")
-        # frame = cv2.resize(frame, (640, 360))
+        frame = cv2.resize(frame, (640, 360), interpolation=cv2.INTER_AREA)
         if not ret:
             break
-        # facecasc = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        # faces = facecasc.detectMultiScale(
-        #    gray, scaleFactor=1.3, minNeighbors=12)
-        faces = face_recognition.face_locations(frame, model="cnn")
-
+        faces = face_recognition.face_locations(frame, model="hog")
         for (y1, x1, y2, x2) in faces:
             cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
             roi_gray = gray[y1:y2, x2:x1]
-            print(f'{x1} {x2} {y1} {y2} {roi_gray}')
-            cv2.imshow('roi', roi_gray)
             cropped_img = np.expand_dims(np.expand_dims(
                 cv2.resize(roi_gray, (48, 48)), -1), 0)
             prediction = model.predict(cropped_img)
             maxindex = int(np.argmax(prediction))
             cv2.putText(frame, emotion_dict[maxindex], (x2, y2),
                         cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-
-            cv2.imshow('Video', frame)
+        cv2.imshow('Video', cv2.resize(frame, (1280, 720)))
+        timestamp = datetime.now().strftime("%Y%m%d%H%M%S%f") + ".jpg"
+        cv2.imwrite(timestamp, frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-
     cap.release()
     cv2.destroyAllWindows()
-
-# emotions will be displayed on your face from the webcam feed
-elif mode == "directory":
+elif mode == "video_whole":
     model.load_weights('model.h5')
-
-    # prevents openCL usage and unnecessary logging messages
+    emotions_array = [0, 0, 0, 0, 0, 0, 0]
     cv2.ocl.setUseOpenCL(False)
-
-    # dictionary which assigns each label an emotion (alphabetical order)
     emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful",
                     3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
-
-    # start the webcam feed
-    inputPath = r"D:\Programing\Projects\Emotion-detection\src\to_sort"
+    cap = cv2.VideoCapture("c.mp4")
+    while True:
+        ret, frame = cap.read()
+        frame = cv2.resize(frame, (640, 360), interpolation=cv2.INTER_AREA)
+        if not ret:
+            break
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_recognition.face_locations(frame, model="hog")
+        for (y1, x1, y2, x2) in faces:
+            roi_gray = gray[y1:y2, x2:x1]
+            cropped_img = np.expand_dims(np.expand_dims(
+                cv2.resize(roi_gray, (48, 48)), -1), 0)
+            prediction = model.predict(cropped_img)
+            maxindex = int(np.argmax(prediction))
+            emotions_array[maxindex] = emotions_array[maxindex] + 1
+        print(emotions_array)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    print(emotions_array)
+    cap.release()
+    cv2.destroyAllWindows()
+elif mode == "directory":
+    model.load_weights('model.h5')
+    cv2.ocl.setUseOpenCL(False)
+    emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful",
+                    3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
+    inputPath = r"to_sort"
     filesArray = []
     for path, subdirs, files in os.walk(inputPath):
         for name in files:
@@ -159,36 +110,29 @@ elif mode == "directory":
     print(filesArray)
     print(len(filesArray))
     for file in filesArray:
+        emotions_array = [0, 0, 0, 0, 0, 0, 0]
         print(file)
         frame = cv2.imread(file)
-        cv2.imshow('Initial', frame)
-        # Find haar cascade to draw bounding box around face
-        frame = cv2.imread(file)
-        cv2.imshow('Initial', frame)
-
-        # frame = cv2.resize(frame, (640, 360))
-        # facecasc = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+        try:
+            real_frame = frame.copy()
+        except:
+            break
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
         faces = face_recognition.face_locations(frame, model="hog")
-
+        print(faces)
         for (y1, x1, y2, x2) in faces:
             cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
             roi_gray = gray[y1:y2, x2:x1]
-            print(f'{x1} {x2} {y1} {y2}')
-
-            cv2.imshow('roi', roi_gray)
-
             cropped_img = np.expand_dims(np.expand_dims(
                 cv2.resize(roi_gray, (48, 48)), -1), 0)
-
             prediction = model.predict(cropped_img)
             maxindex = int(np.argmax(prediction))
+            emotions_array[maxindex] = emotions_array[maxindex] + 1
             cv2.putText(frame, emotion_dict[maxindex], (x2, y2),
                         cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-
-        cv2.imshow('Video', frame)
-
-        cv2.waitKey()
-
+        if (faces == []):
+            cv2.imwrite("sorted\\"+"None_"+file.split("\\")[-1], frame)
+        else:
+            cv2.imwrite(
+                "sorted\\"+emotion_dict[np.argmax(emotions_array)]+"_"+file.split("\\")[-1], frame)
     cv2.destroyAllWindows()
---
+++ insta_scraper.py
@@ -0,0 +1,74 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
+from bs4 import BeautifulSoup
+import requests
+import urllib.request
+import time
+
+# Set the Chrome options
+chrome_options = Options()
+chrome_options.add_argument("--disable-notifications")  # Disable notifications
+chrome_options.add_argument("--disable-infobars")  # Disable infobars
+chrome_options.add_argument("--mute-audio")  # Mute audio
+
+# Set up the ChromeDriver service and WebDriver
+# Replace with the path to your ChromeDriver executable
+service = Service('path/to/chromedriver.exe')
+driver = webdriver.Chrome(service=service, options=chrome_options)
+
+# Set the waiting time for the elements to load (adjust as needed)
+wait_time = 3
+
+
+def scrape_images_from_profile(profile_url, num_scrolls):
+    # Load the profile page
+    driver.get(profile_url)
+    time.sleep(wait_time)
+
+    # Dismiss the notification prompt if it appears
+    try:
+        notif_prompt = driver.find_element(
+            By.XPATH, '//div[@data-testid="tweetbox"]')
+        actions = ActionChains(driver)
+        actions.move_to_element(notif_prompt).click().perform()
+        time.sleep(wait_time)
+    except:
+        pass
+
+    # Parse and download images at each scroll step
+    for _ in range(num_scrolls):
+        print(_)
+        # Get the page source
+        page_source = driver.page_source
+
+        # Parse the page source with BeautifulSoup
+        soup = BeautifulSoup(page_source, 'html.parser')
+
+        # Find and scrape the images
+        tweet_elements = soup.find_all('article', {'role': 'article'})
+        for idx, tweet_element in enumerate(tweet_elements):
+            image_elements = tweet_element.find_all('img', {'src': True})
+            for image_idx, element in enumerate(image_elements):
+                image_url = element['src']
+                # Replace '_normal' in the image URL to get the original resolution image
+                image_url = image_url.replace('_normal', '')
+                # Check if the image width is greater than 600 pixels
+                headers = requests.head(image_url).headers
+                if int(headers.get('content-length', 0)) > 600:
+                    # Download the image using urllib.request
+                    urllib.request.urlretrieve(
+                        image_url, f"scraped\\image_{idx}_{image_idx}.jpg")
+
+        # Scroll down to load more tweets
+        driver.execute_script(
+            "window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(wait_time)
+
+
+profile_url = "https://twitter.com/PokiBruh"  # "https://twitter.com/pokimanelol"
+num_scrolls = 100  # Number of times to scroll the feed
+
+scrape_images_from_profile(profile_url, num_scrolls)
