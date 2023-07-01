d7d9ee539300f4d4c90c9c18f461e9bd9539177f
Umer Irfan <54197021+Foxunderground0@users.noreply.github.com>
2023-07-01 18:53:21 +0500

    Added Logging

--- Node/index.js
+++ Node/index.js
@@ -6,39 +6,42 @@ app.set("view engine", "ejs");
 //app.use(express.static(__dirname + "/public"));
 
 var files_list = []
-var filesystem_root =  __dirname + "/../Immages/"
+var filesystem_root = __dirname + "/../Immages/"
 
-const emotion_dict = ["Angry","Disgusted","Fearful","Happy","Neutral","Sad","Surprised"];
+const emotion_dict = ["Angry", "Disgusted", "Fearful", "Happy", "Neutral", "Sad", "Surprised"];
 const max_loop_count = 100;
 
 fs.readdir(filesystem_root + "display/", (err, files) => {
 	files.forEach(file => {
-	  //console.log(file);
-	  files_list.push(file);
+		//console.log(file);
+		files_list.push(file);
 	});
 	console.log(files_list);
 });
 
 app.get("/", function (req, res) {
-	if(req.query.emotion == undefined){
-		var file = files_list[Math.floor(Math.random()*files_list.length)];
-		res.sendFile("display/" + file, { root : filesystem_root});
+	var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+	console.log(ip);
+	if (req.query.emotion == undefined) {
+		var file = files_list[Math.floor(Math.random() * files_list.length)];
+		console.log(file)
+		res.sendFile("display/" + file, { root: filesystem_root });
 	} else {
 		txt = req.query.emotion;
 		txt = txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-		if(emotion_dict.includes(txt)){
-			var file = files_list[Math.floor(Math.random()*files_list.length)];
+		if (emotion_dict.includes(txt)) {
+			var file = files_list[Math.floor(Math.random() * files_list.length)];
 			console.log(file)
 			var loop_count = 0;
-			while (txt != file.split("_")[0]){
-				if(loop_count >= max_loop_count){
+			while (txt != file.split("_")[0]) {
+				if (loop_count >= max_loop_count) {
 					res.status(501).send("Couldnt find a match");
 				} else {
-					var file = files_list[Math.floor(Math.random()*files_list.length)];
+					var file = files_list[Math.floor(Math.random() * files_list.length)];
 					loop_count++;
 				}
 			}
-			res.sendFile("display/" + file, { root : filesystem_root});
+			res.sendFile("display/" + file, { root: filesystem_root });
 		} else {
 			//console.log("Here")
 			res.status(422).send("Invalid Emotion");
